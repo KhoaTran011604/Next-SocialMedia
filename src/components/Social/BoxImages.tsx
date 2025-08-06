656bd0fcfaf2a6480c1328161c7ddf3d3e259c31
KhoaTran160401 <tranvankhoa.ag.01@gmail.com>
2025-08-06 13:53:12 +0700

    fix number hidden image in boxImages

--- src/components/Social/BoxImages.tsx
+++ src/components/Social/BoxImages.tsx
@@ -18,33 +18,34 @@ const BoxImages = ({ images }: { images: imageProps[] }) => {
       <PhotoProvider>
         {images?.length > 0 &&
           images.map((img, idx) => {
-            if (idx <= 3)
-              return (
-                <PhotoView key={Math.random()} src={img.imageAbsolutePath}>
-                  {idx < 3 ? (
+            return (
+              <PhotoView key={Math.random()} src={img.imageAbsolutePath}>
+                {idx < 3 ? (
+                  <img
+                    src={img.imageAbsolutePath}
+                    style={{ objectFit: "cover" }}
+                    alt="Post"
+                    className="h-auto max-h-[500px] w-full object-cover"
+                  />
+                ) : (
+                  <div className="relative max-h-[500px] w-full overflow-hidden">
+                    {/* Overlay layer */}
+                    {images.length > 4 && (
+                      <div className="absolute inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
+                        <span className="text-2xl font-bold text-white">{`+${images.length - 4}`}</span>
+                      </div>
+                    )}
+
+                    {/* Image layer */}
                     <img
                       src={img.imageAbsolutePath}
-                      style={{ objectFit: "cover" }}
                       alt="Post"
-                      className="h-auto max-h-[500px] w-full object-cover"
+                      className={`z-0 max-h-[500px] w-full object-cover ${idx >= 4 && "hidden"}`}
                     />
-                  ) : (
-                    <div className="relative max-h-[500px] w-full overflow-hidden">
-                      {/* Overlay layer */}
-                      <div className="absolute inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
-                        <span className="text-2xl font-bold text-white">{`+${images.length - 3}`}</span>
-                      </div>
-
-                      {/* Image layer */}
-                      <img
-                        src={img.imageAbsolutePath}
-                        alt="Post"
-                        className="z-0 max-h-[500px] w-full object-cover"
-                      />
-                    </div>
-                  )}
-                </PhotoView>
-              );
+                  </div>
+                )}
+              </PhotoView>
+            );
           })}
       </PhotoProvider>
     </div>
