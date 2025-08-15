f50e267b9a14abbfb778d6e20968db3338028e5a
KhoaTran160401 <tranvankhoa.ag.01@gmail.com>
2025-08-15 14:56:30 +0700

    fix bug build 1

--- src/app/social/edit-post/page.tsx
+++ src/app/social/edit-post/page.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import PostList from "./PostList";
-import NewPost from "./NewPost";
-import QuickInfo from "./QuickInfo";
 import MainPage from "./MainPage";
 
 const page = () => {
